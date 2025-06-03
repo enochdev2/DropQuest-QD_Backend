19296239ff28478bc129b8256c4573cd58af21ce
enochdev2 <enochthechristepistle@gmail.com>
2025-06-03 15:01:42 +0100

    adding admin functionality

--- routes/buyOrderRoutes.js
+++ routes/buyOrderRoutes.js
@@ -11,19 +11,21 @@ import {
   getUserInProgressOrders,
 } from "../controllers/buyOrderController.js";
 
-import { authenticate } from '../middleware/autheticate.js';
+import { authenticate, authorizeAdmin } from '../middleware/autheticate.js';
 
 const router = express.Router();
-
+//? USER ROUTES
 router.post("/", authenticate, createBuyOrder); // Create new buy order (user)
-router.get("/admin/buy-orders/pending", getPendingBuyOrders); // Admin view pending
-router.post("/admin/buy-orders/:orderId/approve", approveBuyOrder); // Admin approve
-router.post("/admin/buy-orders/:orderId/reject", rejectBuyOrder); // Admin reject
 router.get("/buy-orders", authenticate, getUserBuyOrders); // Get user buy orders with optional status filter
 router.get("/user/inProgress-orders", authenticate, getUserInProgressOrders); // Get user buy orders with optional status filter
-router.get("/admin/all/onbuy-orders", getAllOnBuyOrders);
-router.get("/admin/all/pending-orders", getAllPendingBuyApprovalOrders);
-router.get("/admin/all/inProgress-orders", getAllInProgressApprovalOrders);
+
+//? USER ROUTES
+router.get("/admin/buy-orders/pending",authenticate, authorizeAdmin, getPendingBuyOrders); // Admin view pending
+router.post("/admin/buy-orders/:orderId/approve",authenticate, authorizeAdmin, approveBuyOrder); // Admin approve
+router.post("/admin/buy-orders/:orderId/reject",authenticate, authorizeAdmin, rejectBuyOrder); // Admin reject
+router.get("/admin/all/onbuy-orders",authenticate, authorizeAdmin, getAllOnBuyOrders);
+router.get("/admin/all/pending-orders",authenticate, authorizeAdmin, getAllPendingBuyApprovalOrders);
+router.get("/admin/all/inProgress-orders", authenticate, authorizeAdmin, getAllInProgressApprovalOrders);
 
 export default router;
  
