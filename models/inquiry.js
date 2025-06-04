774221f45b053ecde26c77d9ee30e2354db29213
enochdev2 <enochthechristepistle@gmail.com>
2025-06-04 08:20:26 +0100

    adding admin functionality

--- models/inquiry.js
+++ models/inquiry.js
@@ -7,7 +7,7 @@ const inquirySchema = new Schema(
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // the user who made inquiry
     title: { type: String, required: true },
     description: { type: String, required: true },
-    comment: { type: String, default: "", select: false }, // admin only editable, hide by default
+    comment: { type: String, default: "" }, // admin only editable, hide by default
     date: { type: Date, default: Date.now },
     status: {
       type: String,
