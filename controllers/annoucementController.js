153a6ddf5079e748caf5f742c778ee0ce5588135
enochdev2 <enochpromise.va@gmail.com>
2025-08-27 12:40:25 +0100

    adding functionality

--- controllers/annoucementController.js
+++ controllers/annoucementController.js
@@ -3,12 +3,7 @@ import { userModel } from "../models/userModel.js"; // Assuming you have a user 
 
 // Function to create a new announcement
 export const createAnnouncement = async (req, res) => {
-  const { 
-    title, 
-    titlekorean, 
-    content, 
-    contentkorean 
-  } = req.body;
+  const { title, titlekorean, content, contentkorean } = req.body;
   // const createdBy = req.user._id; // Assuming the admin is logged in
 
   try {
@@ -33,13 +28,8 @@ export const createAnnouncement = async (req, res) => {
 
 export const EditAnnouncement = async (req, res) => {
   const { announcementId } = req.params;
-    const { 
-    title, 
-    titlekorean, 
-    content, 
-    contentkorean 
-  } = req.body;
-  
+  const { title, titlekorean, content, contentkorean } = req.body;
+
   try {
     // Find the announcement by ID and update it
     const announcement = await announcementModel.findByIdAndUpdate(
@@ -66,7 +56,9 @@ export const EditAnnouncement = async (req, res) => {
 // Function to get all announcements
 export const getAllAnnouncements = async (req, res) => {
   try {
-    const announcements = await announcementModel.find();
+    const announcements = await announcementModel
+      .find()
+      .sort({ createdAt: -1 });
     res.status(200).json(announcements);
   } catch (error) {
     console.error("Error fetching announcements:", error);
@@ -99,13 +91,18 @@ export const deleteAnnouncement = async (req, res) => {
   const { announcementId } = req.params;
 
   try {
-    const announcement = await announcementModel.findById(announcementId)
+    const announcement = await announcementModel.findById(announcementId);
     if (!announcement) {
       return res.status(404).json({ error: "Announcement not found" });
     }
-    
-    const announcementDeleted = await announcementModel.findByIdAndDelete(announcementId)
-    console.log("🚀 ~ deleteAnnouncement ~ announcementDeleted:", announcementDeleted)
+
+    const announcementDeleted = await announcementModel.findByIdAndDelete(
+      announcementId
+    );
+    console.log(
+      "🚀 ~ deleteAnnouncement ~ announcementDeleted:",
+      announcementDeleted
+    );
     res.status(200).json({ message: "Announcement deleted successfully" });
   } catch (error) {
     console.error("Error deleting announcement:", error);
